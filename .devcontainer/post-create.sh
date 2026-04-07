2785300af6e7d0ab463cd451b18953389be06761
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2026-04-07 17:19:35 +0000

    add protoc-gen

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,3 +1,4 @@
-cd /workspaces/frontend && bun install 
 go install github.com/sqlc-dev/sqlc/cmd/sqlc@latest
 go install github.com/amacneil/dbmate@latest
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
