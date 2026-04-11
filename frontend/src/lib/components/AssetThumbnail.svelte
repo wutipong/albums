d8bd3cf6aa9d5fdc8779690c96ebb0678a4708f7
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2026-04-11 14:03:41 +0000

    add video/animation badge

--- frontend/src/lib/components/AssetThumbnail.svelte
+++ frontend/src/lib/components/AssetThumbnail.svelte
@@ -7,6 +7,9 @@
     let thumbnailWidth = $state(0)
     let thumbnailHeight = $state(0)
     let available = $state(false)
+    let videoDuration = $state('')
+    let imageFrames = $state(1)
+    let assetType = $state('image')
 
     let preview = $state(false)
 
@@ -20,28 +23,43 @@
         thumbnailWidth = obj.thumbnail_width * ratio
         thumbnailHeight = TARGET_HEIGHT
         available = obj.available
+
+        assetType = obj.type
+        videoDuration = obj.video_duration.toString()
+        imageFrames = obj.image_frames
+
+        console.log(obj)
     })
 </script>
 
 <div 
     role='button'
     tabindex='0'
-    class={`h-[${thumbnailHeight}px] rounded-xl m-1 overflow-hidden`} 
+    class={`block h-[${thumbnailHeight}px] rounded-xl m-1 overflow-hidden`} 
     style={`width: ${thumbnailWidth}px;`}
     onmouseenter={()=>preview = true}
     onmouseleave={()=>preview = false}
 >
     <a 
+        class='block'
         href={available? `/api/asset/${id}/view/`: ''} 
         class:disabled={!available} 
     >
-        <img 
-            width={thumbnailWidth}
-            height={thumbnailHeight}  
-            src={`/api/asset/${id}/thumbnail`} 
-            alt='{id}'
-            class:hidden={preview}
-        >   
+    <div class='relative w-full h-full'>
+        <div 
+            class:hidden={preview} 
+            class='rounded-xl overflow-hidden w-full h-full box-border'
+            style={`width: ${thumbnailWidth}px; height: ${thumbnailHeight}px;`}
+            >
+            <img 
+                width={thumbnailWidth}
+                height={thumbnailHeight}  
+                src={`/api/asset/${id}/thumbnail`} 
+                alt='{id}'
+                class:hidden={preview}
+            >   
+        </div>
+
         <div 
             class:hidden={!preview} 
             class='border-4 rounded-xl overflow-hidden w-full h-full box-border'
@@ -54,6 +72,16 @@
                 alt='{id}'
                 class='w-full h-full'
             >
+        </div>
+
+        <div class='absolute top-1 right-2 place-items-end'>
+            {#if assetType === 'video'}
+                <div class='badge' >Video</div>
+            {/if}
+
+            {#if imageFrames > 1}
+                <div class='badge'> Animation</div>
+            {/if}
         </div>
     </a>
 </div>
