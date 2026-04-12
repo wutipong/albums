c339dd6c49032beaa92807db01ceea7f26c92cbc
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2026-04-13 01:12:23 +0700

    refactor ItemViewer and album page layout for improved structure and styling

--- frontend/src/lib/components/ItemViewer.svelte
+++ frontend/src/lib/components/ItemViewer.svelte
@@ -2,8 +2,8 @@
 	let { id } = $props();
 </script>
 
-<div class="absolute top-0 right-0 bottom-0 left-0 backdrop-blur-xl backdrop-brightness-50">
-	<div class="absolute m-auto">
-		<img src={`/api/asset/${id}/view`} alt={id} />
+<div class="absolute top-0 bottom-0 left-0 right-0 backdrop-brightness-50 backdrop-blur-lg">
+	<div class="w-full h-full">
+		<img src={`/api/asset/${id}/view`} alt={id} class="max-w-full max-h-full m-auto"/>
 	</div>
 </div>
--- frontend/src/routes/album/[id]/+page.svelte
+++ frontend/src/routes/album/[id]/+page.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
 	import AssetThumbnail from '$lib/components/AssetThumbnail.svelte';
-	import ItemViewer from '$lib/components/ItemViewer.svelte'
+	import ItemViewer from '$lib/components/ItemViewer.svelte';
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
 </script>
 
-<div class="text-xl font-bold">Album {data.name}</div>
+<div class="relative h-screen w-screen flex flex-col">
+	<div class="text-xl font-bold">Album {data.name}</div>
 
-<div class="flex flex-wrap bg-base-300">
-	{#each data.assets as asset (asset)}
-		<AssetThumbnail id={asset}></AssetThumbnail>
-	{/each}
+	<div class="overflow-auto">
+		<div class="flex flex-wrap bg-base-300">
+			{#each data.assets as asset (asset)}
+				<AssetThumbnail id={asset}></AssetThumbnail>
+			{/each}
+		</div>
+	</div>
+	<ItemViewer id={data.assets[0]}></ItemViewer>
 </div>
-
-<ItemViewer id={data.assets[0]}></ItemViewer>
