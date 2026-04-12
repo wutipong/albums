40b44479a4ecbac694348004d7ccf2628e84039a
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2026-04-12 20:39:35 +0000

    add close button

--- frontend/src/lib/components/ItemViewer.svelte
+++ frontend/src/lib/components/ItemViewer.svelte
@@ -5,7 +5,12 @@
 {#if show}
 	<div class="absolute top-0 right-0 bottom-0 left-0 backdrop-blur-lg backdrop-brightness-50">
 		<div class="h-full w-full">
-			<img src={`/api/asset/${id}/view`} alt={id} class="m-auto max-h-full max-w-full" />
+			<img src={`/api/asset/${id}/view`} alt={id} class="m-auto h-full w-full object-contain" />
 		</div>
+
+		<button 
+			class="btn btn-ghost absolute top-4 right-4 rounded-full" 
+			onclick={() => show = false}>Close
+		</button>
 	</div>
 {/if}
