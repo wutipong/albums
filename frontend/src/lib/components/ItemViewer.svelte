2226af3a699991657e90185eee405c9c58488029
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2026-04-12 21:04:42 +0000

    add icons.

--- frontend/src/lib/components/ItemViewer.svelte
+++ frontend/src/lib/components/ItemViewer.svelte
@@ -1,20 +1,30 @@
 <script lang="ts">
+	import { mdiClose, mdiDotsVertical } from '@mdi/js';
+	import Icon from 'mdi-svelte';
 	let { id, show = $bindable(false) } = $props();
 </script>
 
 {#if show}
-	<div class="absolute top-0 right-0 bottom-0 left-0 backdrop-blur-lg backdrop-brightness-50">
+	<div
+		role="presentation"
+		class="absolute top-0 right-0 bottom-0 left-0 backdrop-blur-lg backdrop-brightness-50"
+	>
 		<div class="h-full w-full">
 			<img src={`/api/asset/${id}/view`} alt={id} class="m-auto h-full w-full object-contain" />
 		</div>
 
-		<button class="btn absolute top-4 right-4 btn-lg btn-circle btn-ghost" onclick={() => (show = false)}>
-			Close
+		<button
+			class="btn absolute top-4 right-4 btn-circle btn-ghost btn-lg"
+			onclick={() => (show = false)}
+		>
+			<Icon path={mdiClose} />
 		</button>
 
 		<div class="fab">
 			<!-- a focusable div with tabindex is necessary to work on all browsers. role="button" is necessary for accessibility -->
-			<div tabindex="0" role="button" class="btn btn-circle btn-ghost btn-lg">:</div>
+			<div tabindex="0" role="button" class="btn btn-circle btn-ghost btn-lg">
+				<Icon path={mdiDotsVertical}/>
+			</div>
 
 			<!-- buttons that show up when FAB is open -->
 			<button class="btn btn-circle btn-lg">A</button>
