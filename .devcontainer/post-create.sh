9c00bdc127b28146754b8dca51153032fb312f36
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2026-04-13 14:42:11 +0000

    use devcontainer to install dependencies

--- .devcontainer/post-create.sh
+++
@@ -1,7 +0,0 @@
-apt update -y
-apt install -y libvips-dev libvpx9 libopus0 ffmpeg
-
-go install github.com/sqlc-dev/sqlc/cmd/sqlc@latest
-go install github.com/amacneil/dbmate@latest
-go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
