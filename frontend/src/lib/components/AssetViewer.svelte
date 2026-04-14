55ccd692580323e257a739068eebedfda88fce7c
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2026-04-14 11:14:41 +0000

    style: enhance AssetViewer component layout and add video player styling

--- frontend/src/lib/components/AssetViewer.svelte
+++ frontend/src/lib/components/AssetViewer.svelte
@@ -34,31 +34,42 @@
 				title={asset.original}
 				src={`/api/asset/${asset.id}/view`}
 			>
-				<media-provider class="h-full w-full object-contain"></media-provider>
+				<media-provider></media-provider>
 				<media-video-layout></media-video-layout>
 			</media-player>
 		{/if}
-		<button
-			class="btn absolute top-1/2 left-4 btn-circle -translate-y-1/2 btn-ghost btn-lg"
-			class:btn-disabled={!hasPrevious}
-			onclick={() => {
-				previous();
-			}}
+		<div
+			class="absolute top-1/2 left-4 -translate-y-1/2 bg-transparent"
+			data-theme={asset.type === 'video' ? 'dark' : null}
 		>
-			<Icon path={mdiChevronLeft} />
-		</button>
-
-		<button
-			class="btn absolute top-1/2 right-4 btn-circle -translate-y-1/2 btn-ghost btn-lg"
-			class:btn-disabled={!hasNext}
-			onclick={() => {
-				next();
-			}}
+			<button
+				class="btn btn-circle btn-ghost btn-lg"
+				class:btn-disabled={!hasPrevious}
+				onclick={() => {
+					previous();
+				}}
+			>
+				<Icon path={mdiChevronLeft} />
+			</button>
+		</div>
+		<div
+			class="absolute top-1/2 right-4 -translate-y-1/2 bg-transparent"
+			data-theme={asset.type === 'video' ? 'dark' : null}
 		>
-			<Icon path={mdiChevronRight} />
-		</button>
-
-		<div class="bg-opacity-100 absolute top-4 right-4 flex flex-row-reverse gap-4 rounded-full">
+			<button
+				class="btn btn-circle btn-ghost btn-lg"
+				class:btn-disabled={!hasNext}
+				onclick={() => {
+					next();
+				}}
+			>
+				<Icon path={mdiChevronRight} />
+			</button>
+		</div>
+		<div
+			class="absolute top-4 right-4 flex flex-row-reverse gap-4 rounded-full bg-transparent"
+			data-theme={asset.type === 'video' ? 'dark' : null}
+		>
 			<button class="btn btn-circle btn-ghost btn-lg" onclick={() => (show = false)}>
 				<Icon path={mdiClose} />
 			</button>
