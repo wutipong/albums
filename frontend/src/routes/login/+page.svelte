672535a898cc27a2e619ca92246f59424d029bc4
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2026-04-15 20:07:37 +0000

    use login button

---
+++ frontend/src/routes/login/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import {authClient} from '$lib/auth-client'
+    async function performLogin() {
+        
+        await authClient.signIn.oauth2({
+            providerId: "generic-oauth",
+            callbackURL: "/album",
+            errorCallbackURL: "/error",
+            // disableRedirect: true,
+        })
+    
+    }
+</script>
+
+<div class="w-screen h-screen">
+    <div class="flex w-full h-full justify-center items-center"> 
+    <button class="btn btn-primary btn-xl "
+        onclick={()=>performLogin()}
+    >
+        Login
+    </button>
+    </div>
+</div>
