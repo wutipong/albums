22288d3a7080b866c32b38a9464b984947b8d294
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2026-04-21 04:57:42 +0000

    refactor: update not available SVG and enhance album cover loading logic

--- frontend/src/lib/components/AlbumItem.svelte
+++ frontend/src/lib/components/AlbumItem.svelte
@@ -1,18 +1,21 @@
 <script lang="ts>
+    import notAvailableSvg from '$lib/assets/not-available-small.svg?raw'
     let {album} = $props()
-
-    const IMAGE_WIDTH = 300
-    const IMAGE_HEIGHT = 200
 </script>
 
-<div class='card bg-base-100 w-[300px] h-[300px] hover:bg-base-300 hover:shadow-xl m-4'>
+<div class='card bg-base-100 w-[300px] h-[300px] border-base-300 border-1 hover:bg-base-300 hover:shadow-xl m-4'>
     <figure>
         <a href={`/album/${album.id}/`}>
-            <img 
-                src={`/api/album/${album.id}/cover`} 
-                alt={album.id} 
-                width={IMAGE_WIDTH} 
-                height={IMAGE_HEIGHT}>
+            {#if album.cover_url == ''}
+                {@html notAvailableSvg}
+            {:else}
+                <img 
+                    src={album.cover_url} 
+                    alt={album.id} 
+                    width='300' 
+                    height='200'
+                    >
+            {/if}
         </a>
     </figure>
     <div class='card-body h-4 overflow-clip'>
--- frontend/src/routes/album/+page.server.ts
+++ frontend/src/routes/album/+page.server.ts
@@ -1,16 +1,35 @@
+import { env } from "$env/dynamic/private";
 import { db } from "$lib/server/db";
-import type { PageLoad } from "./$types";
+import type { PageServerLoad } from "./$types";
 import { generateImageUrl } from '@imgproxy/imgproxy-node'
 
-export const load: PageLoad = async ({ params, fetch }) => {
-	const res = await fetch('/api/album');
-	const data = await res.json();
-
+export const load: PageServerLoad = async () => {
 	const albums = await db.selectFrom("albums")
 		.selectAll()
 		.where('deleted_at', 'is', null)
 		.orderBy('name')
 		.execute()
 
-	return { albums };
+	const outAlbums = []
+	for (const album of albums) {
+		let cover_url = ""
+		if (album.cover != "") {
+			cover_url = generateImageUrl({
+				endpoint: env.IMGPROXY_URL,
+				url: `s3://${env.S3_BUCKET}/${album.cover}`,
+				options: {
+					resizing_type: "auto",
+					height: 200,
+					width: 300,
+					enlarge: 1,
+				},
+				salt: env.IMGPROXY_SALT,
+				key: env.IMGPROXY_KEY,
+			})
+		}
+
+		outAlbums.push({ ...album, cover_url })
+	}
+
+	return { albums: outAlbums };
 };
