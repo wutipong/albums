0cd2bc649e7ba58e9c05920e57e201394e43c2ba
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2026-07-31 21:46:48 +0000

    chore: reorganize imports in +layout.svelte for clarity

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	// This injects Temporal onto the global window/globalThis object
+	import 'temporal-polyfill/global';
 
 	let { children } = $props();
+
+
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
