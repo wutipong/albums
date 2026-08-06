5b9c0308dec96878b3a54dee65d39e3bc893d101
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2026-08-06 14:14:11 +0000

    feat: enhance admin statistics and processing queue display

--- frontend/src/routes/admin/+page.server.ts
+++ frontend/src/routes/admin/+page.server.ts
@@ -1,26 +1,66 @@
-import { db } from "$lib/server/db";
-import type { PageServerLoad } from "./$types";
+import { db } from '$lib/server/db';
+import { sql } from 'kysely';
+import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
-    const count = await db.selectFrom("assets").select((eb) => [eb.fn.countAll().as("count")]).executeTakeFirst();
-    const pendings = await db.selectFrom("assets").
-        leftJoin("albums", "assets.album_id", "albums.id").
-        select('assets.id as asset_id').
-        select('albums.name as album_name').
-        select('assets.created_at as created_at').
-        where("process_status", "=", "pending").
-        orderBy("assets.created_at", "asc").
-        limit(20).execute();
+	const count = await db
+		.selectFrom('assets')
+		.select((eb) => [eb.fn.countAll().as('count')])
+		.executeTakeFirst();
 
-    const pendingCount = await db.selectFrom("assets").select((eb) => [eb.fn.countAll().as("count")]).where("process_status", "=", "pending").executeTakeFirst();
-    const failedCount = await db.selectFrom("assets").select((eb) => [eb.fn.countAll().as("count")]).where("process_status", "=", "failed").executeTakeFirst();
-    const album_count = await db.selectFrom("albums").select((eb) => [eb.fn.countAll().as("count")]).executeTakeFirst();
+	const pendingCount = await db
+		.selectFrom('assets')
+		.select((eb) => [eb.fn.countAll().as('count')])
+		.where('process_status', '=', 'pending')
+		.executeTakeFirst();
 
-    return { 
-        count: count ? BigInt(count.count) : 0n, 
-        pendings, 
-        failedCount: failedCount ? BigInt(failedCount.count) : 0n,
-        pendingCount: pendingCount ? BigInt(pendingCount.count) : 0n,
-        albumCount: album_count ? BigInt(album_count.count) : 0n,
-     };
-}
+	const failedCount = await db
+		.selectFrom('assets')
+		.select((eb) => [eb.fn.countAll().as('count')])
+		.where('process_status', '=', 'failed')
+		.executeTakeFirst();
+
+	const album_count = await db
+		.selectFrom('albums')
+		.select((eb) => [eb.fn.countAll().as('count')])
+		.executeTakeFirst();
+
+	const queue = await db
+		.selectFrom('neoq_jobs')
+		.where('status', '=', 'new')
+		.where(sql<string>`payload->>'command'`, '=', 'process-asset')
+		.orderBy('id', 'asc')
+		.limit(20)
+		.selectAll()
+		.execute();
+
+	let queueItems = [];
+	for (const job of queue) {
+		if (!job.payload) {
+			continue;
+		}
+
+		const payload = job.payload as any;
+		const asset_id = payload.id;
+		const asset = await db
+			.selectFrom('assets')
+			.leftJoin('albums', 'assets.album_id', 'albums.id')
+			.select('assets.id as asset_id')
+            .select('type')
+			.select('albums.name as album_name')
+            .select('album_id')
+			.select('assets.created_at as created_at')
+			.where('assets.id', '=', asset_id)
+			.executeTakeFirst();
+		if (asset) {
+			queueItems.push(asset);
+		}
+	}
+	return {
+		count: count ? BigInt(count.count) : 0n,
+		queueItems: queueItems,
+		failedCount: failedCount ? BigInt(failedCount.count) : 0n,
+		pendingCount: pendingCount ? BigInt(pendingCount.count) : 0n,
+		albumCount: album_count ? BigInt(album_count.count) : 0n
+	};
+};
--- frontend/src/routes/admin/+page.svelte
+++ frontend/src/routes/admin/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import NavBar from '$lib/components/NavBar.svelte';
 	import type { PageProps } from './$types';
+    import Icon from 'mdi-svelte';
+    import {mdiInformationOutline} from '@mdi/js';
 
 	let { data }: PageProps = $props();
 	const formatter = new Intl.NumberFormat('en-US', {
@@ -8,11 +10,14 @@
 		compactDisplay: 'short',
 		maximumFractionDigits: 1 // Controls decimal points (e.g., 1.5K vs 1.52K)
 	});
+
+	$inspect(data);
 </script>
 
 <div class="relative flex h-screen w-screen flex-col">
 	<NavBar />
 	<div class="overflow-auto p-4 pt-8">
+        <h2 class="text-2xl font-semibold">Statistics</h2>
 		<div class="stats shadow">
 			<div class="stat">
 				<div class="stat-title">Total Assets</div>
@@ -24,15 +29,21 @@
 				<div class="stat-value text-primary">{formatter.format(data.pendingCount)}</div>
 			</div>
 
-            <div class="stat">
-                <div class="stat-title">Failed Assets</div>
-                <div class="stat-value text-error">{formatter.format(data.failedCount)}</div>
-            </div>
+			<div class="stat">
+				<div class="stat-title">Failed Assets</div>
+				<div class="stat-value text-error">{formatter.format(data.failedCount)}</div>
+			</div>
 
 			<div class="stat">
 				<div class="stat-title">Total Albums</div>
 				<div class="stat-value text-primary">{formatter.format(data.albumCount)}</div>
 			</div>
+		</div>
+
+		<h2 class="text-2xl font-semibold">Processing Queue</h2>
+		<div role="alert" class="alert alert-info">
+			<Icon path={mdiInformationOutline} />
+			<span>This table shows up to 20 items being wait in the queue.</span>
 		</div>
 
 		<div class="overflow-x-auto">
@@ -41,34 +52,37 @@
 					<tr>
 						<th>#</th>
 						<th>ID</th>
+						<th>Type</th>
 						<th>Albums</th>
 						<th>Created At</th>
 					</tr>
 				</thead>
 				<tbody>
-					{#if data.pendingCount === 0n}
+					{#if data.queueItems.length === 0}
 						<tr>
-							<td colspan="4" class="text-center">No pending assets found.</td>
+							<td colspan="5" class="text-center">No processing assets found.</td>
 						</tr>
 					{:else}
-						{#each data.pendings as asset, index}
+						{#each data.queueItems as asset, index}
 							<tr>
 								<th>{index + 1}</th>
 								<td>{asset.asset_id}</td>
-								<td>{asset.album_name}</td>
+								<td>{asset.type}</td>
+								<td
+									><a href={`album/${asset.album_id}`} class="link link-primary"
+										>{asset.album_name}</a
+									></td
+								>
 								<td>{asset.created_at.toLocaleString()}</td>
 							</tr>
 						{/each}
-
-						<tr>
-							<td colspan="4" class="text-center">Some pending assets are not displayed.</td>
-						</tr>
 					{/if}
 				</tbody>
 				<tfoot>
 					<tr>
 						<th>#</th>
 						<th>ID</th>
+						<th>Type</th>
 						<th>Albums</th>
 						<th>Created At</th>
 					</tr>
