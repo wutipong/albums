877151c003c591d79998df8cc9769003818ff18e
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2026-08-20 17:20:51 +0000

    feat: implement admin panel with albums and queue management, including statistics and processing details

--- frontend/src/routes/admin/+layout.svelte
+++ frontend/src/routes/admin/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import NavBar from '$lib/components/NavBar.svelte';
+	import { mdiImageAlbum, mdiTrayFull } from '@mdi/js';
+	import Icon from 'mdi-svelte';
 
 	let { children } = $props();
 </script>
@@ -7,15 +9,14 @@
 <NavBar />
 <div class="drawer lg:drawer-open">
 	<input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
-	<div class="drawer-content flex flex-col items-center justify-center">
+	<div class="drawer-content">
 		{@render children()}
 	</div>
 	<div class="drawer-side">
 		<label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"></label>
 		<ul class="menu min-h-full w-80 bg-base-200 p-4">
-			<!-- Sidebar content here -->
-			<li><a>Sidebar Item 1</a></li>
-			<li><a>Sidebar Item 2</a></li>
+			<li><a href="/admin/albums"><Icon path={mdiImageAlbum} />Albums</a></li>
+			<li><a href="/admin/queue"><Icon path={mdiTrayFull} /> Processing Queue</a></li>
 		</ul>
 	</div>
 </div>
