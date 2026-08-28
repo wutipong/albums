3bd1759d19435d17c4cf55c60d68863479fe90fa
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2026-08-28 23:40:43 +0000

    feat(user): integrate avatar generation and clean up API key management

--- frontend/src/routes/user/+page.server.ts
+++ frontend/src/routes/user/+page.server.ts
@@ -1,8 +1,13 @@
 import type { PageServerLoad } from './$types';
-
+import {SHA256} from 'bun'
 export const load: PageServerLoad = async ({ locals }) => {
+
+	const hashVal = SHA256.hash(locals.user.email, 'hex')
+	const avatarSrc = `https://gravatar.com/avatar/${hashVal} `;
+
 	return {
 		user: locals.user,
-		session: locals.session
+		session: locals.session,
+		avatarSrc
 	};
 };
